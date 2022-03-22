0a59fe645e99d4411c1787c4ca59e2b5f314f486
NahataManami <manami.nahata@rakus-partners.co.jp>
2022-03-22 10:50:05 +0900

    UserMapperクラスに下記の処理を追加 ・ユーザー登録 ・取得したidと一致するユーザー情報の取得 ・取得したメールアドレスとパスワードに一致するユーザー情報の取得 ・ユーザー情報の更新

--- src/main/java/com/example/demo/controller/TestController.java
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.example.demo.domain.User;
 import com.example.demo.repository.UserMapper;
 
 @Controller
@@ -13,11 +14,53 @@ public class TestController {
 	@Autowired
 	private UserMapper userMapper;
 	
-	@RequestMapping("/find-all")
-	public String findAll() {
+//	@RequestMapping("/insertUser")
+//	public String insert() {
+//		
+//		User user = new User();
+//		
+//		user.setUserName("まな");
+//		user.setUserEmail("mana@sample.com");
+//		user.setUserPassword("manamana123");
+//		
+//		userMapper.insertUser(user);
+//		
+//		return "redirect:/users/find-all";
+//	}
+//	
+//	@RequestMapping("/findAll")
+//	public String findAll() {
+//		
+//		System.out.println("findAll：");
+//		System.out.println(userMapper.findAll());
+//		
+//		return "OK";
+//	}
+//	
+//	@RequestMapping("/findById")
+//	public String findByID() {
+//		System.out.println("findById：");
+//		System.out.println(userMapper.findById(1));
+//		return "OK";
+//	}
+//	
+//	@RequestMapping("/findByEmailAndPassword")
+//	public String findByEmailAndPassword() {
+//		System.out.println("findByEmailAndPassword：");
+//		System.out.println(userMapper.findByEmailAndPassword("tarou@sample.com", "taro1234"));
+//		return "OK";
+//	}
+	
+	@RequestMapping("/updateUser")
+	public String updateUser() {
+		User user = new User();
+		user.setUserId(1);
+		user.setUserPassword("tarou1234");
+
+		userMapper.updateUser(user);
 		
-		System.out.println("ユーザーの全件検索：");
-		System.out.println(userMapper.findAll());
+		System.out.println("updateUser：");
+		System.out.println(userMapper.findById(1));
 		
 		return "OK";
 	}
