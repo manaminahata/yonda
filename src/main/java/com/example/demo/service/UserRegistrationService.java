500b054a07d27719aa3af24c5848dd1649e90acf
NahataManami <manami.nahata@rakus-partners.co.jp>
2022-03-24 17:00:32 +0900

    ユーザー登録処理用のServiceクラスの作成完了 ・findByEmail：メールの重複チェック ・insertUser：ユーザー情報の登録

--- src/main/java/com/example/demo/service/UserRegistrationService.java
+++ src/main/java/com/example/demo/service/UserRegistrationService.java
@@ -1,8 +1,34 @@
 package com.example.demo.service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.example.demo.domain.User;
+import com.example.demo.repository.UserMapper;
 
 @Service
 public class UserRegistrationService {
-
+	
+	@Autowired
+	private UserMapper userMapper;
+	
+	/**
+	 * 登録済みのメールアドレスかチェックする
+	 * @return
+	 */
+	public List<User> findByEmail(String email) {
+		List<User> userList = userMapper.findAll();
+		List<User> user = userList.stream()
+			.filter(i -> i.getUserEmail().equals(email))
+			.collect(Collectors.toList());
+		return user;
+	}
+	
+	public void insertUser(User user) {
+		userMapper.insertUser(user);
+	}
+	
 }
