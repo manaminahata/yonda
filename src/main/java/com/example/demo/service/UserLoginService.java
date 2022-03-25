6582dd4fc233af1190119d944ca5279159169082
NahataManami <manami.nahata@rakus-partners.co.jp>
2022-03-25 10:36:37 +0900

    UserLoginServiceの作成完了 ・loginUser：引数のメールアドレスとパスワードと一致するユーザー情報を取得する

--- src/main/java/com/example/demo/service/UserLoginService.java
+++ src/main/java/com/example/demo/service/UserLoginService.java
@@ -1,8 +1,24 @@
 package com.example.demo.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.example.demo.domain.User;
+import com.example.demo.repository.UserMapper;
 
 @Service
 public class UserLoginService {
 
+	@Autowired
+	private UserMapper userMapper;
+	
+	/**
+	 * メールアドレスとパスワードが一致したユーザーを取得する
+	 * @param email
+	 * @param password
+	 * @return 引数のメールアドレスとパスワードと一致するユーザー情報
+	 */
+	public User loginUser(String email, String password) {
+		return userMapper.findByEmailAndPassword(email, password);
+	}
 }
